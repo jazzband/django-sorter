9e5f5bf101cce569561a4dfd3924f2781cb4a182
Jannis Leidel <jannis@leidel.info>
2011-09-01 15:24:44 +0200

    Extended settings.

--- sorter/models.py
+++ sorter/models.py
@@ -1,6 +1,10 @@
-from django.conf import settings
 from appconf import AppConf
 
 class SorterConf(AppConf):
-    RAISE_EXCEPTIONS = settings.DEBUG
+    RAISE_EXCEPTIONS = None
     QUERY_NAME = 'sort'
+    EVALUATE_AFTERWARDS = True
+
+    def configure_raise_exceptions(self, value):
+        from django.conf import settings
+        return settings.DEBUG
